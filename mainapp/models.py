15c402d343154dd5868bc92978f8ba54b6034929
Matt Beiswenger <mattbeis@yahoo.com>
2018-01-29 00:29:12 -0600

    created a janky toolbar at the bottom of the editor and added a not so functioning fullscreen mode

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 
 class Assignment(models.Model):
     title = models.CharField(max_length=128, unique=True)
-    points = models.IntegerField(default=0)
-    description = models.TextField()
-    due_date = models.DateField()
-    time = models.TimeField()
-    inputs = models.TextField()
+    points = models.IntegerField(default=0, blank=True)
+    description = models.TextField(blank=True)
+    due_date = models.DateField(blank=True)
+    time = models.TimeField(blank=True)
+    inputs = models.TextField(blank=True)
     function_name = models.CharField(max_length=128)
     slug = models.SlugField(unique=True)
     instructions_file = models.FileField(blank=True, null=True,
