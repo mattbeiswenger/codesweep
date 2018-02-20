0ad9974ad30fd9f540b61855114d426a1752808e
Matt Beiswenger <mattbeis@yahoo.com>
2018-02-19 18:47:05 -0600

    Completed upload file feature for the editor

---
+++ media/code/mbeiswenger--Homework+1--2018-02-19--00-44-27.py
@@ -0,0 +1,13 @@
+def function(num):
+	#insert code here
+	sdfd
+
+import sys
+
+def Main():
+	with open(sys.argv[1], 'r') as input, open(sys.argv[2], 'w') as output:
+		for line in input:
+			output.write(str(function(int(line))) + '\n')
+
+if __name__ == '__main__':
+	Main()
